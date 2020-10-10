ffee5f8be36cf934f0bd1489aea433e39356d11a
Cara Zellner <carajeanine@gmail.com>
2020-10-10 18:59:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,42 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    create = self.new
+    create.save
+    create
+  end
+  
+  def self.new_by_name(name)
+    new_by_name = self.new 
+    new_by_name.name = name
+    new_by_name
+  end
+  
+  def self.create_by_name(name)
+    create_by_name = self.new 
+    create_by_name.name = name 
+    create_by_name.save
+    create_by_name
+  end
+  
+  def self.find_by_name(name)
+    @@all.find do |song|
+      song.name == name 
+    end
+  end
+  
+  def self.find_or_create_by_name(name)
+    @@all.find do |song|
+      song == song
+    end
+    self.find_by_name(name) ||
+    self.create_by_name(name)
+  end
+  
+  def self.alphabetical
+    self.sort_by(name)
+  end
 
 end
